6173279727f815c6b0d8e503b8bf38dead2e4bd8
Matthew Foale <foalematthew@gmail.com>
2014-09-21 19:37:53 +0100

    Postgres commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 AuthexampleWebapp::Application.routes.draw do
   devise_for :users
+  
+namespace :api do
+  namespace :v1 do
+    devise_scope :user do
+       post 'registrations' => 'registrations#create', :as => 'register'
+      post 'sessions' => 'sessions#create', :as => 'login'
+      delete 'sessions' => 'sessions#destroy', :as => 'logout'
+    end
+  end
+end
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
