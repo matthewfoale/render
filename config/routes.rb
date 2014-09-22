5227120ade0e692c80cbb4562e4e5008125a2ae7
Matthew Foale <foalematthew@gmail.com>
2014-09-22 17:01:15 +0100

    to get it to render

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ namespace :api do
       post 'sessions' => 'sessions#create', :as => 'login'
       delete 'sessions' => 'sessions#destroy', :as => 'logout'
     end
+     get 'tasks' => 'tasks#index', :as => 'tasks'
   end
 end
 
